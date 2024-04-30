c0aa8c3dafee723c9b9c1a7add7511900e8aa048
ry754729 <168565172+ry754729@users.noreply.github.com>
2024-04-30 11:52:00 -0400

    Update R06_MET01_J.java

--- R06_MET01_J.java
+++ R06_MET01_J.java
@@ -1,4 +1,11 @@
 public static int getAbsAdd(int x, int y) {
-    return Math.abs(x) + Math.abs(y);
+  if (x == Integer.MIN_VALUE || y == Integer.MIN_VALUE) {
+    throw new IllegalArgumentException();
   }
-  getAbsAdd(Integer.MIN_VALUE, 1);
+  int absX = Math.abs(x);
+  int absY = Math.abs(y);
+  if (absX > Integer.MAX_VALUE - absY) {
+    throw new IllegalArgumentException();
+  }
+  return absX + absY;
+}
