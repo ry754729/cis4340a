aa9879b79aa9e4deda550d9754e5fc2e3e6aa9bd
ry754729 <168565172+ry754729@users.noreply.github.com>
2024-04-30 11:54:52 -0400

    Update R11_TPS03_J.java

--- R11_TPS03_J.java
+++ R11_TPS03_J.java
@@ -1,15 +1,31 @@
 final class PoolService {
-    private final ExecutorService pool = Executors.newFixedThreadPool(10);
-   
-    public void doSomething() {
-      pool.execute(new Task());
-    }
+  // The values have been hard-coded for brevity
+  ExecutorService pool = new CustomThreadPoolExecutor(
+      10, 10, 10, TimeUnit.SECONDS, new ArrayBlockingQueue<Runnable>(10));
+  // ...
+}
+ 
+class CustomThreadPoolExecutor extends ThreadPoolExecutor {
+  // ... Constructor ...
+  public CustomThreadPoolExecutor(
+      int corePoolSize, int maximumPoolSize, long keepAliveTime,
+      TimeUnit unit, BlockingQueue<Runnable> workQueue) {
+    super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue);
   }
-   
-  final class Task implements Runnable {
-    @Override public void run() {
-      // ...
-      throw new NullPointerException();
-      // ...
+ 
+ 
+  @Override
+  public void afterExecute(Runnable r, Throwable t) {
+    super.afterExecute(r, t);
+    if (t != null) {
+      // Exception occurred, forward to handler
     }
+    // ... Perform task-specific cleanup actions
   }
+ 
+  @Override
+  public void terminated() {
+    super.terminated();
+    // ... Perform final clean-up actions
+  }
+}
