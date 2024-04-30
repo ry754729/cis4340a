bfbe66e496349ea6ef551e648d50338e8e0c9963
ry754729 <168565172+ry754729@users.noreply.github.com>
2024-04-30 11:52:19 -0400

    Update R07_ERR02_J.java

--- R07_ERR02_J.java
+++ R07_ERR02_J.java
@@ -1,6 +1,6 @@
 try {
-    // ...
-  } catch (SecurityException se) {
-    System.err.println(se);
-    // Recover from exception
-  }
+  // ...
+} catch(SecurityException se) {
+  logger.log(Level.SEVERE, se);
+  // Recover from exception
+}
